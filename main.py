9a5d5cd2fd2e6d08272b36b9d780ab1a7bd69a16
Benjamin Nass <benjanass@gmail.com>
2023-10-14 11:04:10 -0300

    uploading to github

--- main.py
+++ main.py
@@ -16,21 +16,20 @@ reddit = praw.Reddit(
     password=password,
 )
 
-for submission in reddit.subreddit("rickandmorty").hot(limit=10):
-    print(submission.title)
-""" # Specify the subreddit to monitor
+""" for submission in reddit.subreddit("rickandmorty").hot(limit=10):
+    print(submission.title) """
+# Specify the subreddit to monitor
 subreddit_name = 'rickandmorty'  # Change this to the specific subreddit you want
 
 # Search query
 search_query = 'Season 7'
 
 # Monitor new submissions
-subreddit = reddit.subreddit(subreddit_name) """
+subreddit = reddit.subreddit(subreddit_name)
 
 
-""" for submission in subreddit.new(limit=10):
+for submission in subreddit.new(limit=10):
     if search_query.lower() in submission.title.lower():
         # You've found new information about Season 7, report it!
         print("New Season 7 information found:", submission.title)
         # Implement your reporting logic here (e.g., send a notification).
- """
