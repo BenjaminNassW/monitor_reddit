0e74dfebbd65063ca7eff5aa731c3b6c1e6d04be
Benjamin Nass <benjanass@gmail.com>
2023-10-14 13:21:45 -0300

    action button

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import praw
-from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
-load_dotenv()
 
 # Reddit API credentials
-client_id = str(os.getenv('CLIENT_ID'))
-client_secret = str(os.getenv('CLIENT_SECRET'))
-user_agent = str(os.getenv('USER_AGENT'))
-username = str(os.getenv('USERNAME_REDDIT'))
-password = str(os.getenv('PASSWORD'))
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+user_agent = os.getenv('USER_AGENT')
+username = os.getenv('USERNAME_REDDIT')
+password = os.getenv('PASSWORD')
 
 # Initialize the Reddit API client
 reddit = praw.Reddit(
